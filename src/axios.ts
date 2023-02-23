dc80257aefbb18f8df51dab6b64a7c1270d39d34
Denys1994q <drybachok@gmail.com>
2023-02-23 19:18:49 +0200

    baseUrl norm

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: process.env.REACT_APP_API_URL
-    baseURL: 'https://blog-backend1994.onrender.com'
+    baseURL: process.env.REACT_APP_API_URL
+    // baseURL: 'https://blog-backend1994.onrender.com'
 })
 
 // функція яка перевіряє при кожному запиті не важливо якому чи ми авторизовані (чи є токен в локалстореджі)
